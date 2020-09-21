847ee6e7933c4c972540c9c89b8973e0728d7d8b
Lukasz Chalinski <l.chalinski@gmail.com>
2020-09-21 14:36:31 +0200

    dodalem dzialajace przyciski do dodawania i aktualizowania statusu zadania

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -12,11 +14,13 @@ class Task(db.Model):
     description = db.Column(db.String(200))
     complete = db.Column(db.Boolean)
 
+
 @app.route("/")
 def index():
     #  pokaż wszystkie zadania
     todo_list = Task.query.all()
     return render_template('base.html', todo_list=todo_list)
+
 
 @app.route("/add", methods=["POST"])
 def add():
@@ -28,6 +32,23 @@ def add():
     db.session.commit()
     return redirect(url_for("index"))
 
+
+@app.route("/update/<int:task_id>")
+def update(task_id):
+    # zaktualizuje zadanie
+    task = Task.query.filter_by(id=task_id).first()
+    task.complete = not task.complete
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
+@app.route("/delete/<int:task_id>")
+def delete(task_id):
+    # zaktualizuje zadanie
+    task = Task.query.filter_by(id=task_id).first()
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
